3eced04723ea4a6de71c3e182a46458b45ce4b7b
Milad Khajavi <khajavi@gmail.com>
2013-05-12 12:36:41 +0430

    add signals

--- main.cpp
+++ main.cpp
@@ -15,6 +15,9 @@ tictactoe::tictactoe() {
 			int id = i*3 + x;
 			boxes[i]->add( *buttonHolder[id] );
 			buttonHolder[id]->set_size_request( 100, 40 );
+
+			buttonHolder[id]->signal_clicked().connect(sigc::bind<Gtk::Button*>(sigc::mem_fun(*this, &tictactoe::buttonPressed), buttonHolder[id]));
+
 			buttonHolder[id]->show();
 		}
 		boxes[i]->show();
@@ -33,6 +36,55 @@ tictactoe::tictactoe() {
 	label.show();
 	vertical.show();
 }
+
+
+void tictactoe::buttonPressed(Gtk::Button * button){
+	if(winner == 1)
+		return;
+	if(turn == 0){
+		button->set_label("X");
+		button->set_sensitive(false);
+		label.set_label("O's turn");
+		turn = 1;
+	}else{
+		turn = 0;
+		button->set_label("O");
+		button->set_sensitive(false);
+		label.set_label("X's turn");
+	}
+
+	//Check Diagonals
+	if(buttonHolder[0]->get_label() != "" && buttonHolder[0]->get_label() == buttonHolder[4]->get_label() && buttonHolder[4]->get_label() == buttonHolder[8]->get_label()){
+		label.set_label(buttonHolder[0]->get_label() + " wins");	
+			winner = 1;
+	}
+
+	if(buttonHolder[2]->get_label() != "" && buttonHolder[2]->get_label() == buttonHolder[4]->get_label() && buttonHolder[6]->get_label() == buttonHolder[8]->get_label()){
+		label.set_label(buttonHolder[2]->get_label() + " wins");	
+			winner = 1;
+	}
+
+
+	//Check horizontals and verticals
+	for(int i = 0; i < 3; i++){
+		if(buttonHolder[i*3]->get_label() != "" && buttonHolder[i*3 + 0]->get_label() == buttonHolder[i*3 + 1]->get_label() && buttonHolder[i*3 + 1]->get_label() == buttonHolder[i*3 + 2]->get_label() ){
+			label.set_label(buttonHolder[i*3 + 0]->get_label() + " wins");
+			winner = 1;
+		}
+
+		if(buttonHolder[i]->get_label() != "" && buttonHolder[i]->get_label() == buttonHolder[i + 3]->get_label() && buttonHolder[i+ 3]->get_label() == buttonHolder[i+ 6]->get_label()){
+			label.set_label(buttonHolder[i]->get_label() + " wins");
+			winner = 1;
+		}
+	}
+
+	numOff ++;
+
+	if(numOff == 9){
+		label.set_label("No one wins");
+	}
+}
+
 
 tictactoe::~tictactoe() {
 }
